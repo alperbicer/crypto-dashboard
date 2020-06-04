a159331452ed2fc387f025cfed7842783b60b9fd
Alper Bicer <alperbicer@yandex.com>
2020-06-04 18:57:15 +0300

    added utility

---
+++ src/utils/utility.js
@@ -0,0 +1,8 @@
+const isEmpty = function (obj) {	
+  for (let key in obj) {	
+    if (Object.prototype.hasOwnProperty.call(obj, key))	
+      return false;	
+  }	
+  return true;	
+};	
+export { isEmpty }; 
